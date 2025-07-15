dc8fbbfb0a66d87081761c54e60c47c22a21f5bb
N Balaji <154417002+itsmebalu221@users.noreply.github.com>
2025-07-15 19:39:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const multer = require("multer");
 const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
+require("dotenv").config();
+
 
 const app = express();
-const PORT =5001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
